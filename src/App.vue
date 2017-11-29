fee157e92eeecb0682ee0910b4ed15fa1a16b187
Burricornio <alejandro_web@hotmail.com>
2017-11-29 20:37:03 +0100

    Subo la solución

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+        <h1>VueSortable</h1>
+          <ul v-sortable class="list-group">
+            <li class="list-group-item">Foo</li>
+            <li class="list-group-item">Bar</li>
+            <li class="list-group-item">Baz</li>
+          </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Vue from 'vue'
+import Sortable from 'sortablejs'
+
+Vue.directive('sortable', {
+  inserted: function (el, binding) {
+    new Sortable(el, binding.value || {})
+  }
+})
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
